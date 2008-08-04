154c86bea4c59597a2e52eb8f7a40f6dfc0151bb
Benjamin C Meyer <ben@meyerhome.net>
2008-08-04 21:30:56 +0200

    Rather then trying to parse the file let webkit do it and just walk the dom with javascript.

--- tools/htmlToXBel/htmlToXBel.pro
+++ tools/htmlToXBel/htmlToXBel.pro
@@ -6,7 +6,7 @@ INCLUDEPATH += .
 win32: CONFIG += console
 mac:CONFIG -= app_bundle
 
-QT += network
+QT += network webkit
 
 # Input
 SOURCES += main.cpp
@@ -15,3 +15,6 @@ RCC_DIR     = $$PWD/.rcc
 UI_DIR      = $$PWD/.ui
 MOC_DIR     = $$PWD/.moc
 OBJECTS_DIR = $$PWD/.obj
+
+RESOURCES += source.qrc
+
--- tools/htmlToXBel/main.cpp
+++ tools/htmlToXBel/main.cpp
@@ -18,14 +18,21 @@
  */
 
 #include <QtGui/QtGui>
+#include <QtWebKit/QtWebKit>
 
+/*!
+    A tool to convert html bookmark files into the xbel format.
+
+    The html bookmark files should be DOCTYPE: NETSCAPE-Bookmark-file-1
+
+    More information about XBel can be found here: http://pyxml.sourceforge.net/topics/xbel/
+*/
 int main(int argc, char **argv)
 {
-    QCoreApplication application(argc, argv);
+    QApplication application(argc, argv);
 
     QFile inFile;
     QFile outFile;
-    QXmlStreamWriter writer;
     QTextStream in(&inFile);
     QTextStream out(&outFile);
 
@@ -35,87 +42,46 @@ int main(int argc, char **argv)
     bool setInput = false;
     bool setOutput = false;
     QStringList args = application.arguments();
+    args.pop_front();
     foreach (QString arg, args) {
         if (arg == QLatin1String("-o")) {
             setOutput = true;
         } else if (setOutput) {
             outFile.setFileName(arg);
             outFile.open(QIODevice::WriteOnly);
-            writer.setDevice(&outFile);
         } else if (QFile::exists(arg)) {
             setInput = true;
             inFile.setFileName(arg);
             inFile.open(QIODevice::ReadOnly);
         } else {
-            qWarning() << "Usage:" << argv[0]
+            qWarning() << "Usage: htmlToXBel"
                         << "[stdin|htmlfile]" << "[stdout|-o outFile]";
             return 1;
         }
     }
 
-    if (!setInput) inFile.open(stdin, QIODevice::ReadOnly);
-    if (!setOutput) {
+    if (!setInput)
+        inFile.open(stdin, QIODevice::ReadOnly);
+    if (!setOutput)
         outFile.open(stdout, QIODevice::WriteOnly);
-        writer.setDevice(&outFile);
-    }
     if (inFile.openMode() == QIODevice::NotOpen
-        || outFile.openMode() == QIODevice::NotOpen)
+        || outFile.openMode() == QIODevice::NotOpen) {
+        qWarning() << "Unable to open streams";
         return 1;
+    }
 
-    writer.setAutoFormatting(true);
-    writer.writeStartDocument();
-    writer.writeDTD(QLatin1String("<!DOCTYPE xbel>"));
-    writer.writeStartElement(QLatin1String("xbel"));
-    writer.writeAttribute(QLatin1String("version"), QLatin1String("1.0"));
-
-    // NETSCAPE-Bookmark-file-1 DTD
-
-    do {
-        // Folder
-        QString line = in.readLine().trimmed();
-        if (line.startsWith("<DT><H3 ")) {
-            line = line.mid(7);
-            int endOfH3Tag = line.indexOf('>');
-            QString h3 = line.mid(0, endOfH3Tag);
-            bool folded = h3.contains("FOLDED");
-            QString title = line.mid(endOfH3Tag + 1, line.indexOf('<') - endOfH3Tag - 1);
-
-            writer.writeStartElement(QLatin1String("folder"));
-            writer.writeAttribute(QLatin1String("folded"), folded ? QLatin1String("yes") : QLatin1String("no"));
-            writer.writeTextElement(QLatin1String("title"), title);
-            continue;
-        }
-        if (line.contains("</DL>")) {
-            writer.writeEndElement();
-            continue;
-        }
-
-        // Bookmark
-        if (line.startsWith("<DT><A HREF=")
-            || line.startsWith("<DT><H3><A HREF=")) {
-            int startURL = line.indexOf('"') + 1;
-            int endURL = line.indexOf('"', startURL);
-            if (startURL == -1 || endURL == -1)
-                continue;
-            QString url = line.mid(startURL, endURL - startURL);
-            int startTitle = line.indexOf('>', endURL) + 1;
-            int endTitle = line.indexOf('<', startTitle);
-            QString title = line.mid(startTitle, endTitle - startTitle);
-
-            writer.writeStartElement(QLatin1String("bookmark"));
-            writer. writeAttribute(QLatin1String("href"), url);
-            writer.writeTextElement(QLatin1String("title"), title);
-            writer.writeEndElement();
-
-        }
-
-        // separator
-        if (line.contains("<HR>")) {
-            writer.writeEmptyElement(QLatin1String("separator"));
-        }
-    } while (!in.atEnd());
-    writer.writeEndDocument();
-
+    QWebView webView;
+    webView.setHtml(inFile.readAll());
+    QFile jsFile(":/extract.js");
+    if (!jsFile.open(QFile::ReadOnly)) {
+        qWarning() << "Unable to load javascript to extract bookmarks.";
+        return 1;
+    }
+    QString result = webView.page()->mainFrame()->evaluateJavaScript(jsFile.readAll()).toString();
+    if (result.isEmpty()) {
+        qWarning() << "Error while extracting bookmarks.";
+        return 1;
+    }
+    out << result;
     return 0;
 }
-
